a6abec3e5b3be8688d9b5333324f294f3a0b430d
umingming <u_0112@naver.com>
2023-05-25 08:28:55 +0900

    build

--- client/src/components/main/MainCalendar.vue
+++ client/src/components/main/MainCalendar.vue
@@ -45,4 +45,57 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.main-calendar {
+    position: absolute;
+    z-index: 1 !important;
+    left: 0;
+    width: 60%;
+    height: 100%;
+    background: rgba(73, 120, 250, 0.097);
+}
+:deep .vc-pane-header-wrapper {
+    width: 25% !important;
+    left: 50%;
+    transform: translateX(-50%);
+}
+:deep .vc-title {
+    font-size: 18px;
+}
+:deep .vc-monthly .is-not-in-month * {
+    opacity: 0.5;
+}
+:deep .vc-weekday {
+    position: relative;
+    transform: translate(29px, 3px);
+    font-size: 15px;
+    opacity: 0.5;
+}
+:deep .vc-day {
+    background: white;
+    margin: 5px;
+    height: 80px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+:deep .vc-day .vc-day-content {
+    font-size: 15px;
+    font-weight: 500 !important;
+    transform: translate(29px, -25px);
+    width: 25px;
+    height: 25px;
+}
+:deep .vc-day:hover .vc-day-content {
+    background: #2564eb17;
+}
+:deep .vc-highlight {
+    transform: translate(29px, -24px);
+    background: #2563eb;
+    width: 25px;
+    height: 25px;
+}
+:deep .day-content {
+    font-size: 10px;
+    transform: translate(3px, 2px);
+}
+</style>
