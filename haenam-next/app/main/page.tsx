0a56722ef6119a294ae8df0f63b851bc8537843a
umingming <u_0112@naver.com>
2024-09-02 17:30:20 +0900

    feat: Add calendar

--- haenam-next/app/main/page.tsx
+++ haenam-next/app/main/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+
 export default function Main() {
     return (
         <div className="box" id="main">
             <header>
                 <h1 id="logo">해냄</h1>
             </header>
-            <div className="main-calendar"></div>
+            <div className="main-calendar">
+                <FullCalendar
+                    plugins={[ dayGridPlugin ]}
+                    initialView="dayGridMonth"
+                    dayMaxEvents={1}
+                    events={[
+                        { title: 'event 1', date: '2024-09-01' },
+                        { title: 'event 10', date: '2024-09-08' },
+                        { title: 'event 10', date: '2024-09-08' },
+                        { title: 'event 10', date: '2024-09-15' },
+                        { title: 'event 10', date: '2024-09-15' },
+                        { title: 'event 10', date: '2024-09-01' },
+                        { title: 'event 1', date: '2024-09-01' },
+                    ]}
+                />
+            </div>
             <div className="main-list"></div>
         </div>
     );
